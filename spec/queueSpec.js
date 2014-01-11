730c28171385ebbf80620c1d897044163b2bbeab
BeArnis <berzins555@gmail.com>
2014-01-11 12:59:47 +0200

    put propertie front

--- spec/queueSpec.js
+++ spec/queueSpec.js
@@ -1,7 +1,11 @@
 /*global describe, it, expect, beforeEach, queue_init */
 describe("Queue", function () {
+  var myqueue = queue_init();
 
   it("after initiation should return something", function () {
     expect(typeof (queue_init())).toEqual("object");
+  });
+  it(" should have properties front", function () {
+    expect(myqueue.front).toEqual(0);
   });
 });
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 function queue_init() {
   var queue = {
-
+    front: 0
   };
   return queue;
 }
